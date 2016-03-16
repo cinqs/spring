75dcebd83f7f25862dab22a5f085f2d62b159680
worldline <worldline@localhost.localdomain>
2016-03-16 11:11:14 -0400

    the first version, that would be useful

---
+++ src/main/java/hello/Cisong.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class Cisong{
+
+  private int age;
+  private boolean gender;
+
+  public Cisong(int age, boolean gender){
+    this.age = age;
+    this.gender = gender;
+  }
+
+  public int getAge(){
+    return this.age;
+  }
+
+  public boolean getGender(){
+    return this.gender;
+  }
+}
