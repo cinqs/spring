85e016bfa3ec59cc99db3b5bca79d4e2efcf202c
worldline <worldline@localhost.localdomain>
2016-03-18 11:39:31 -0400

    scheduled work is accomplicated

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,13 +2,13 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application{
 
 	public static void main(String [] args){
-
-		SpringApplication.run(Application.class, args);
-		System.out.println("the server started on http://localhost:8080");
+		SpringApplication.run(Application.class);
 	}
 }
